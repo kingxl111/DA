02a8cc48c688f8563aaa31dd849e56f55515d0d7
kingxl111 <kingxl111@mail.ru>
2024-12-17 16:23:44 +0300

    my_gzip with some keys added(-c -r -d -9)

--- bwt-mtf-rle/bench/v4.cpp
+++ bwt-mtf-rle/bench/v4.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#include <filesystem>
 
+namespace fs = std::filesystem;
 using namespace std;
 
 class SuffixArray {
@@ -286,103 +288,156 @@ void readEncodedData(ifstream& inputFile, string& encodedData, size_t bitCount) 
     }
 }
 
-int main(int argc, char* argv[]) {
+void printUsageInfo() {
+    cerr << "Usage: my_gzip [-cdlrt19] [file...]\n";
+    cerr << "Options:\n"
+         << "  -c   Write output to stdout, keep original files unchanged.\n"
+         << "  -d   Decompress files.\n"
+         << "  -l   List compressed file contents.\n"
+         << "  -k   Keep\n"
+         << "  -r   Process directories recursively.\n"
+         << "  -t   Test compressed file integrity.\n"
+         << "  -1   Fastest (min compression).\n"
+         << "  -9   Best compression.\n";
+}
 
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+void collectFiles(const string& path, vector<string>& files, bool recursive) {
+    if (recursive) {
+        for (const auto& entry : fs::recursive_directory_iterator(path)) {
+            if (fs::is_regular_file(entry.path())) {
+                files.push_back(entry.path().string());
+            }
+        }
+    } else {
+        if (fs::is_regular_file(path)) {
+            files.push_back(path);
+        } else if (!fs::is_directory(path)) {
+            files.push_back(path);
+        } else {
+            cerr << "Error: " << path << " is not a file." << endl;
+        }
+    }
+}
 
-    if (argc < 2) {
-        cerr << "Usage: <compress|decompress> input_file output_file" << endl;
-        return 1;
+// TODO: add light compress and light de without Huffman
+void LiteCompress(const string& inputFilename, const string& outputFilename, bool toStdout) {}
+
+void LiteDecompress(const string& inputFilename, const string& outputFilename, bool toStdout) {}
+
+void Compress(const string& inputFilename, const string& outputFilename, bool toStdout) {
+    ifstream inputFile(inputFilename, ios::binary);
+    if (!inputFile) {
+        cerr << "Error opening input file." << endl;
+    }
+    vector<uint8_t> inputData((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
+    inputFile.close();
+
+    auto [bwtData, primaryIndex] = BWT(inputData);
+    vector<uint8_t> mtfData = MTF(bwtData);
+    vector<uint8_t> rleData = RLE(mtfData);
+
+    vector<int> frequencies(256, 0);
+    for (uint8_t byte : rleData) {
+        frequencies[byte]++;
     }
 
-    string cmd = argv[1];
+    HuffmanNode* root = buildHuffmanTree(frequencies);
+    unordered_map<uint8_t, string> huffmanTable;
+    buildHuffmanTable(root, "", huffmanTable);
 
-    if (cmd == "compress") {
-        if (argc < 4) {
-            cerr << "Usage: compress input_file output_file" << endl;
-            return 1;
-        }
-
-        string inputFilename = argv[2];
-        string outputFilename = argv[3];
-
-        ifstream inputFile(inputFilename, ios::binary);
-        if (!inputFile) {
-            cerr << "Error opening input file." << endl;
-            return 1;
-        }
-
-        vector<uint8_t> inputData((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
-        inputFile.close();
-
-        auto [bwtData, primaryIndex] = BWT(inputData);
-        vector<uint8_t> mtfData = MTF(bwtData);
-        vector<uint8_t> rleData = RLE(mtfData);
-
-        vector<int> frequencies(256, 0);
-        for (uint8_t byte : rleData) {
-            frequencies[byte]++;
-        }
-
-        HuffmanNode* root = buildHuffmanTree(frequencies);
-        unordered_map<uint8_t, string> huffmanTable;
-        buildHuffmanTable(root, "", huffmanTable);
-
-        string encodedData;
-        HuffmanEncode(rleData, encodedData, huffmanTable);
-        // cout << "ENCODED DATA: " << encodedData;
-        ofstream outputFile(outputFilename, ios::binary);
+    string encodedData;
+    HuffmanEncode(rleData, encodedData, huffmanTable);
+    // cout << "ENCODED DATA: " << encodedData;
+    ofstream outputFile(outputFilename, ios::binary | ios::app);
+    if(!toStdout) {
         if (!outputFile) {
             cerr << "Error opening output file." << endl;
-            return 1;
         }
-
-        outputFile.write(reinterpret_cast<const char*>(&primaryIndex), sizeof(primaryIndex));
-        // printf("PRIMARY INDEX: %d\n", primaryIndex);
-        size_t encodedBitCount = encodedData.size();
-        // printf("ENCODED BIT COUNT: %ld\n", encodedBitCount);
-        outputFile.write(reinterpret_cast<const char*>(&encodedBitCount), sizeof(encodedBitCount));
-        writeEncodedData(outputFile, encodedData);
-
-        for (int freq : frequencies) {
-            outputFile.write(reinterpret_cast<const char*>(&freq), sizeof(freq));
-        }
-
-        deleteHuffmanTree(root);
-        outputFile.close();
     }
 
-    else if (cmd == "decompress") {
-        if (argc < 4) {
-            cerr << "Usage: decompress input_file output_file" << endl;
-            return 1;
-        }
+    if (toStdout) {
+        size_t encodedBitCount = encodedData.size(); 
+        cout 
+        << reinterpret_cast<const char*>(&primaryIndex) 
+        << reinterpret_cast<const char*>(&encodedBitCount);
+        uint8_t buffer = 0;
+        int bitCount = 0;
+        for (char bit : encodedData) {
+            buffer <<= 1;
+            if (bit == '1') {
+                buffer |= 1;
+            }
+            bitCount++;
 
-        string inputFilename = argv[2];
-        string outputFilename = argv[3];
-
-        ifstream inputFile(inputFilename, ios::binary);
-        if (!inputFile) {
-            cerr << "Error opening input file." << endl;
-            return 1;
+            if (bitCount == 8) {
+                cout << reinterpret_cast<const char*>(&buffer);
+                buffer = 0;
+                bitCount = 0;
+            }
         }
+        if (bitCount > 0) { 
+            buffer <<= (8 - bitCount);
+            cout << reinterpret_cast<const char*>(&buffer);
+        }
+        deleteHuffmanTree(root);
+        return;
+    }
+
+    outputFile.write(reinterpret_cast<const char*>(&primaryIndex), sizeof(primaryIndex));
+    // printf("PRIMARY INDEX: %d\n", primaryIndex);
+    size_t encodedBitCount = encodedData.size();
+    // printf("ENCODED BIT COUNT: %ld\n", encodedBitCount);
+    outputFile.write(reinterpret_cast<const char*>(&encodedBitCount), sizeof(encodedBitCount));
+    writeEncodedData(outputFile, encodedData);
+
+    for (int freq : frequencies) {
+        outputFile.write(reinterpret_cast<const char*>(&freq), sizeof(freq));
+    }
+
+    deleteHuffmanTree(root);
+    outputFile.close();
+}
+
+void Decompress(const string& inputFilename, const string& outputFilename, bool toStdout) {
+
+    ifstream inputFile(inputFilename, ios::binary);
+    if (!inputFile) {
+        cerr << "Error opening input file." << endl;
+        return;
+    }
+    ofstream outputFile(outputFilename, ios::binary | ios::app);
+    if (!outputFile) {
+        cerr << "Error opening output file." << endl;
+        return;
+    }
+
+    while(!inputFile.eof()) {
         int primaryIndex;
         inputFile.read(reinterpret_cast<char*>(&primaryIndex), sizeof(primaryIndex));
-        // printf("PRIMARY INDEX: %d\n", primaryIndex);
+        if (inputFile.eof()) {
+            return;
+        }
+        if (!inputFile) {
+            cerr << "Error reading primary index." << endl;
+            return;
+        }
+
         size_t encodedBitCount;
         inputFile.read(reinterpret_cast<char*>(&encodedBitCount), sizeof(encodedBitCount));
-        // printf("ENCODED BIT COUNT: %ld\n", encodedBitCount);
+        if (!inputFile) {
+            cerr << "Error reading encoded bit count." << endl;
+            return;
+        }
 
         string encodedData;
         readEncodedData(inputFile, encodedData, encodedBitCount);
-        // cout << "ENCODED DATA: " << encodedData;
+
         vector<int> frequencies(256, 0);
         for (int& freq : frequencies) {
             inputFile.read(reinterpret_cast<char*>(&freq), sizeof(freq));
             if (!inputFile) {
                 cerr << "Error reading frequency table." << endl;
-                return 1;
+                return;
             }
         }
 
@@ -395,22 +450,141 @@ int main(int argc, char* argv[]) {
         vector<uint8_t> mtfDecoded = ReverseMTF(rleDecoded);
         vector<uint8_t> originalData = ReverseBWT(mtfDecoded, primaryIndex);
 
-        ofstream outputFile(outputFilename, ios::binary);
-        if (!outputFile) {
-            cerr << "Error opening output file." << endl;
-            return 1;
-        }
-
         outputFile.write(reinterpret_cast<char*>(originalData.data()), originalData.size());
-        outputFile.close();
-
         deleteHuffmanTree(root);
     }
-    
-    else {
-        cerr << "Invalid command." << endl;
-        return 1;
+
+    outputFile.close();
+    inputFile.close();
+}
+
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 3) {
+        printUsageInfo();
+        return -1;
     }
 
-    return 0;
+    bool compress = true;
+    bool decompress = false;
+    bool toStdout = false;
+    bool list = false; 
+    bool recursive = false;
+    bool test = false;
+    bool minCompress = false;
+    bool maxCompress = true;
+    bool keep = false;
+
+    string inputFile = "-", outputFile;
+    vector<string> inputFiles;
+
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
+        if (arg[0] == '-') {
+            for (char c : arg.substr(1)) {
+                switch (c) {
+                    case 'c': 
+                        toStdout = true; 
+                        break;
+                    case 'd': 
+                        compress = false; 
+                        decompress = true;
+                        break;
+                    case 'l':
+                        list = true;
+                        break;
+                    case 'r':
+                        recursive = true;
+                        break;
+                    case 't':
+                        test = true;
+                        break;
+                    case '1':
+                        maxCompress = false;
+                        minCompress = true;
+                        break;
+                    case '9':
+                        maxCompress = true;
+                        minCompress = false;
+                        break;
+                    case 'k':
+                        keep = true;
+                        break;
+                    default:
+                        cerr << "Wrong option: -" << c << endl; 
+                        return 1;
+                }
+            }
+        } else {
+            if (inputFile == "-") {
+                inputFile = arg;
+                collectFiles(inputFile, inputFiles, recursive);
+            } else if (i + 1 < argc) {
+                collectFiles(arg, inputFiles, recursive);
+            } else {
+                if (toStdout) {
+                    collectFiles(arg, inputFiles, recursive);
+                } else {
+                    if (!fs::is_directory(arg)) {
+                        outputFile = arg;
+                    } else {
+                        cerr << "Error: " << arg << " is not a file." << endl;
+                        return -1;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "compress: " << compress << endl
+         << "decompress: " << decompress << endl
+         << "toStdout: " << toStdout << endl
+         << "list: " << list << endl
+         << "recursive: " << recursive << endl
+         << "test: " << test << endl
+         << "minCompress: " << minCompress << endl
+         << "maxCompress: " << maxCompress << endl
+         << "outputFile: " << outputFile << endl
+         << endl;
+    
+    for (size_t i = 0; i < inputFiles.size(); i++) {
+        cout << "inputFile{" << i + 1 << "}: " << inputFiles[i] << endl; 
+    }
+
+    if (compress) {
+        for (size_t i = 0; i < inputFiles.size(); i++) {
+            if (maxCompress) {
+                Compress(inputFiles[i], outputFile, toStdout);
+            } else {
+                LiteCompress(inputFiles[i], outputFile, toStdout);
+            }
+        }
+    }
+    else if (decompress) {
+        if (maxCompress) {
+            for (size_t i = 0; i < inputFiles.size(); i++) {
+                Decompress(inputFiles[i], outputFile, toStdout);
+            }
+        } else {
+            for (size_t i = 0; i < inputFiles.size(); i++) {
+                LiteDecompress(inputFiles[i], outputFile, toStdout);
+            }
+        }
+    } else {
+        cerr << "Compress or decompress required!" << endl;
+        return -1;
+    }
+
 }
+
+    /*
+    if (outputFile.empty()) {
+        outputFile = compress ? inputFile + ".gz" : inputFile.substr(0, inputFile.rfind('.'));
+    }
+
+    if (compress) {
+        Compress(inputFile, outputFile, toStdout);
+    }
+    */
+
